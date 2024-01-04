b60668c2c4f75ebf992045464961f6a96815be73
zahid ansari <zahid776592@gmail.com>
2024-01-04 20:53:16 +0530

    myblog: Save crate feature

---
+++ src/main/java/com/myblog01/Myblog01Application.java
@@ -0,0 +1,13 @@
+package com.myblog01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myblog01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myblog01Application.class, args);
+	}
+
+}
